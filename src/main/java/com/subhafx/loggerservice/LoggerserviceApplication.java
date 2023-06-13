a1ceeddb6f50b852fe9c0eb798a0d5b6fd80ed1f
subhafx <dasguptafx@gmail.com>
2023-06-13 23:49:19 +0530

    initial commit

---
+++ src/main/java/com/subhafx/loggerservice/LoggerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.subhafx.loggerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerserviceApplication.class, args);
+    }
+
+}
